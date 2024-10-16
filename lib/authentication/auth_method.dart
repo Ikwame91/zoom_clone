7b5428c4edd2965e43758819b446ceecdeaabdc6
Ikwame <iaagyapong@st.knust.edu.gh>
2024-10-16 19:15:15 +0000

    revert: Revert "Update auth_method.dart"

--- lib/authentication/auth_method.dart
+++ lib/authentication/auth_method.dart
@@ -5,9 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:zoom_clone_101/utils/utils.dart';
-final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore=FirebaseFirestore.instance;
-class AuthMethods{
+
+
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore=FirebaseFirestore.instance;
